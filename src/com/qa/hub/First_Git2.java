b5d766f616061b19d7a15e3c1ac04d24c02223ba
HP <HP@DESKTOP-UTC920B>
2023-04-17 12:50:08 +0530

    Added Some Class to BranchX please Check It Once.........

---
+++ src/com/qa/hub/First_Git2.java
@@ -0,0 +1,41 @@
+package com.qa.hub;
+
+public class First_Git2 {
+
+	public static void main(String[] args) {
+		int i = 20;
+		if (i>20) {
+			System.out.println("print the dog");
+		} else {
+			
+
+			
+			
+			
+			
+			
+			System.out.println("print the cat");
+			System.out.println("print the Parrot");
+			System.out.println("print the Snake");
+			
+			
+			
+			
+			
+			
+			System.out.println("print the cat");
+			System.out.println("print the Parrot");
+			System.out.println("print the Snake");
+			
+			
+			
+			
+			
+			System.out.println("print the cat");
+			System.out.println("print the Parrot");
+			System.out.println("print the Snake");
+
+		}
+	}
+
+}
